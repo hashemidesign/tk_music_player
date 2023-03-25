8ea074cc98be296a8082f2a243b73b91b84b7f80
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-03-25 05:49:34 +0330

    add play button

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
+
+def print_sth():
+    print("hello world")
+
+
 window = Tk()
 window.geometry('300x300')
 window.title('TK Music Player')
@@ -10,6 +15,9 @@ text_label.pack()
 play_icon = PhotoImage(file="assets/icons/play.png").subsample(5)
 play_icon_label = Label(window, image=play_icon)
 play_icon_label.pack()
+
+play_btn = Button(window, image=play_icon, command=print_sth)
+play_btn.pack()
 
 if __name__ == "__main__":
     window.mainloop()
